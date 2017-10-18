70011a2f7cc4b971b813133083e801a02b778c29
kraeml <kraeml@sesamestreet.franken.de>
2017-10-18 21:24:37 +0200

    Refactor playbook with db_user variable

--- bin/create-usb.sh
+++ bin/create-usb.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -o xtrace
-set -o
-shopt extglob                   # shows extglob status
-shopt -s extglob                # enables extglob
-shopt extglob                   # shows extglob status
+
+shopt extglob           # shows extglob status
+shopt -s extglob        # enables extglob
+shopt -p                # shows extglob status
+
 BOX_VERSION=virtualbox-ubuntu1604-$(cat VERSION).box
 for i in  $(ls -d /media/michl/PR_*); do
     mkdir ${i}/builds || true
@@ -15,6 +16,6 @@ for i in  $(ls -d /media/michl/PR_*); do
     rsync -av ./Vagrantfile ${i}/
     rsync -av ./downloads/* ${i}/downloads/
 done
-shopt -u extglob                # disables extglob
-shopt extglob                   # shows extglob status
+shopt -u extglob    # disables extglob
+shopt extglob       # shows extglob status
 sync
